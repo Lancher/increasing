70fe4cd9e058e4cd89b0aa3fd9161aabb5e609bb
Steve <steve.liushihao@gmail.com>
2018-01-07 12:25:18 +0800

    replace example.png

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='increasing',
     packages=['increasing'],
-    version='0.1',
+    version='0.2',
     description='Coloring Progress-Bar for Your Command-Line/Terminal Applications',
     author='Shi Hao Liu',
     author_email='Steve.LiuShiHao@gmail.com',
